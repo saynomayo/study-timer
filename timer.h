a75d1b5958870da84f51a38421954473780204cd
mwaples <mwaples@udel.edu>
2024-09-03 16:38:11 -0400

    created some helpers

--- timer.h
+++ timer.h
@@ -1,3 +1,14 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "sense.h"
+
+void open_display(void);
+void close_display(void);
+void clear_display(void);
+
+void display_hour(int h1, int h2);
+void display_minute(int m1, int m2);
+
+void open_input(void);
+void check_input(void (*)(unsigned int code), int delay);
+void close_input(void);
