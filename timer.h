eda86b3f2bb9b7f4c6c7c20d6c45ec4d72673290
mwaples <mwaples@udel.edu>
2024-09-06 00:40:17 -0400

    removed unnecessary functions from sense.h

--- timer.h
+++ timer.h
@@ -8,14 +8,6 @@ void clear_display(void);
 
 void draw_number(int number, int x, int y);
 
-void display_hour1(int h1, int xh1, int yh);
-void display_hour2(int h2, int xh2, int yh);
-void display_hours(int h1, int h2, int xh1, int xh2, int yh);
-
-void display_minute1(int m1, int xm1, int yx);
-void display_minute2(int m2, int xm2, int yx);
-void display_minutes(int m1, int m2, int xm1, int xm2, int yx);
-
 void display_colons(void);
 
 
