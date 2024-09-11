06242a331856329481b022d9ddfff0b7f2ccb1e1
mwaples <mwaples@udel.edu>
2024-09-10 23:39:27 -0400

    defined new colors and created new themes. Will add more themes later..styles on the way :)

--- main.c
+++ main.c
@@ -6,14 +6,19 @@ int THEME = 0, STYLE = 0;
 //SOLID COLORS
 #define BLACK 0x0000
 #define WHITE 0xFFFF
-#define YELLOW 0xFF06
+#define YELLOW 0xFF0A
 #define BLUE 0x00FF
 #define RED 0xF000
 #define PINK 0xF00F
-#define PURPLE 0x60F0
+#define PURPLE 0x88F0
 #define ORANGE 0xF300
+#define GREEN 0x0F06
+#define BROWN 0x4065
+#define MAGENTA 0xAA06
+#define BEIGE 0x6A06
+#define LAVENDER 0x6A0B
 
-#define test 0xFB66
+#define test 0x4065
 //PASTELS/SPECIAL COLORS
 #define MINTGREEN 0x0F0A
 #define SKYBLUE 0x5F5F
@@ -96,11 +101,11 @@ void joystick_handler(unsigned int code) {
 void SET_TIME_STATE(void) {
     check_joystick(joystick_handler, 1);
     clear_display();
-    display_colons(LOWRED);
-    draw_number(m2, 0, 0, PEACH);
-    draw_number(m1, 4, 0, PEACH);
-    draw_number(s2, -1, 4, BLUSH);
-    draw_number(s1, 3, 4, BLUSH);
+    display_colons(test);
+    draw_number(m2, 0, 0, test);
+    draw_number(m1, 4, 0, test);
+    draw_number(s2, -1, 4, test);
+    draw_number(s1, 3, 4, test);
     usleep(25000);
 }
 
@@ -111,27 +116,68 @@ void TIMER_COUNTS_DOWN(int theme) {
         STATE=0;
      }
      else {
-        if (theme==0)
-     {
-        clear_display();
-        //default theme (UDel babyyy)
-        display_colons(WHITE);
-        draw_number(m2, 0, 0, YELLOW);
-        draw_number(m1, 4, 0, YELLOW);
-        draw_number(s2, -1, 4, BLUE);
-        draw_number(s1, 3, 4, BLUE);
+        switch(theme) 
+        {
+            case(0):
+                clear_display();
+                //default theme (UDel babyyy)
+                display_colons(WHITE);
+                draw_number(m2, 0, 0, YELLOW);
+                draw_number(m1, 4, 0, YELLOW);
+                draw_number(s2, -1, 4, BLUE);
+                draw_number(s1, 3, 4, BLUE);
+            break;
+
+            case(1):
+                clear_display();
+                //cherry blossom theme
+                display_colons(PINK);
+                draw_number(m2, 0, 0, BLUSH);
+                draw_number(m1, 4, 0, BLUSH);
+                draw_number(s2, -1, 4, WHITE);
+                draw_number(s1, 3, 4, WHITE);   
+            break;
+
+            case(2):
+                clear_display();
+                //lava theme
+                display_colons(RED);
+                draw_number(m2, 0, 0, ORANGE);
+                draw_number(m1, 4, 0, ORANGE);
+                draw_number(s2, -1, 4, PEACH);
+                draw_number(s1, 3, 4, PEACH);   
+            break;
+
+            case(3):
+                 clear_display();
+                //forest theme
+                display_colons(SKYBLUE);
+                draw_number(m2, 0, 0, GREEN);
+                draw_number(m1, 4, 0, GREEN);
+                draw_number(s2, -1, 4, BROWN);
+                draw_number(s1, 3, 4, BROWN); 
+            break;
+
+            case(4):
+            break;
+            
+            case(5):
+            break;
+
+            case(6):
+            break;
+
+            case(7):
+            break;
+
+            case(8):
+            break;
+
+            case(9):
+            break;
+                
+            }
      }
-        if (theme==1) 
-     {
-        clear_display();
-        //cherry blossom theme
-        display_colons(PINK);
-        draw_number(m2, 0, 0, BLUSH);
-        draw_number(m1, 4, 0, BLUSH);
-        draw_number(s2, -1, 4, WHITE);
-        draw_number(s1, 3, 4, WHITE);
-     }
-    }
     update_timer();
 }
 
